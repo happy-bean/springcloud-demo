99c9cbd73bcc6ec5ac7a2ff51b04d6665cda9456
bean <18222997200@163.com>
2019-01-15 12:03:36 +0800

    Initial commit

---
+++ springcloud-demo-client/src/main/java/org/happybean/springcloud/client/demo/SpringCloudClientApplication.java
@@ -0,0 +1,14 @@
+package org.happybean.springcloud.client.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientApplication.class, args);
+	}
+
+}
+
---
+++ springcloud-demo-server/src/main/java/org/happybean/springcloud/server/demo/SpringCloudServerApplication.java
@@ -0,0 +1,14 @@
+package org.happybean.springcloud.server.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerApplication.class, args);
+	}
+
+}
+

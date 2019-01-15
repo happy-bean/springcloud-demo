4c0600e567ffd36d34adba134944908fd1f5a645
bean <18222997200@163.com>
2019-01-15 17:42:39 +0800

    springcloud 注册中心 服务提供

--- springcloud-demo-client/src/main/java/org/happybean/springcloud/client/demo/SpringCloudClientApplication.java
+++ springcloud-demo-client/src/main/java/org/happybean/springcloud/client/demo/SpringCloudClientApplication.java
@@ -1,14 +1,29 @@
 package org.happybean.springcloud.client.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringCloudClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/test")
+    public String home(@RequestParam(value = "name", defaultValue = "happybean") String name) {
+        return "hi:" + name + " ,i am from port:" + port;
+    }
 
 }
 

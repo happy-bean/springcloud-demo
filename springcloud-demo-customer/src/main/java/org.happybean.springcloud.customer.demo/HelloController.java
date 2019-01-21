a4c11da158734b9ea1879cde2b14027c56e0e9ab
bean <18222997200@163.com>
2019-01-21 15:31:27 +0800

    命名修改

--- springcloud-demo-customer/src/main/java/org.happybean.springcloud.customer.demo/HelloController.java
+++ springcloud-demo-customer/src/main/java/org.happybean.springcloud.customer.demo/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  **/
 @RestController
-public class HelloControler {
+public class HelloController {
 
     @Autowired
     private HelloService helloService;
